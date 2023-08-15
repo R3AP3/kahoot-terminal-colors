f974b52f255a58a3eb262c843b3da5720a4d98d3
R3AP3 <89069925+R3AP3@users.noreply.github.com>
2023-08-15 13:30:23 +0200

    Create wezterm.lua

---
+++ wezterm/wezterm.lua
@@ -0,0 +1,31 @@
+config.colors = {
+  foreground = '#eaeaea',
+  background = 'rgba(15,15,15,0.65)',
+  cursor_bg = '#eaeaea',
+  cursor_fg = 'rgba(15,15,15,1)',
+  selection_fg = 'rgba(15,15,15,1)',
+  selection_bg = '#eaeaea',
+  tab_bar = {
+    inactive_tab_edge = '#f0f0f0',
+  },
+  ansi = {
+    '#363636',
+    '#ff3355',
+    '#66bf39',
+    '#ffc00a',
+    '#45a3e5',
+    '#864cbf',
+    '#33cccc',
+    '#e0e0e0'
+  },
+  brights = {
+    '#727272',
+    '#ff99aa',
+    '#b2df9c',
+    '#ffdd33',
+    '#a2d1f2',
+    '#c2a5df',
+    '#99e5e5',
+    '#ffffff'
+  }
+}
